a430117ae54259edf2c3d1cf28f8d6c5043a55fd
Abhirup Sengupta <217376601+abhirup-2005@users.noreply.github.com>
2025-10-05 01:52:15 +0530

    Add New Features and Improved UI buttons and their work- > change no. of grid: prompts user for number of grids > clear             : clears the pen marks like a full eraser > white/rainbow     : togles between white and random pen color > darking           : changes the opacity of white pen with every                       interaction and turns it black within 10                       interaction HTML, CSS and JS code lines added for these features

--- script.js
+++ script.js
@@ -1,30 +1,98 @@
+let n = 16;
+let color = "white";
 const container = document.querySelector(".container");
+const grid = document.querySelector(".grid");
+const reset = document.querySelector(".reset");
+const penColor = document.querySelector(".penColor");
+const darkPen = document.querySelector(".darker").addEventListener("click", () => {
+    color = "Dark";
+    
+});
 
-let isDrawing = false;
-// Track pointer state
-document.addEventListener("mousedown", () => isDrawing = true);
-document.addEventListener("mouseup", () => isDrawing = false);
 
-for (let i = 1; i <= 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("rowDisplay");
-    container.append(row);
+gridMaker(n);
 
-    for (let j = 1; j <= 16; j++) {
-        const col = document.createElement("div");
-        col.classList.add("columnDisplay");
+function gridMaker(n) {
+    let isDrawing = false;
+    // Track pointer state
+    document.addEventListener("mousedown", () => isDrawing = true);
+    document.addEventListener("mouseup", () => isDrawing = false);
+    for (let i = 1; i <= n; i++) {
+        const row = document.createElement("div");
+        row.classList.add("rowDisplay");
+        container.append(row);
 
-        //color first square
-        col.addEventListener("mousedown", () => {
-            col.style.backgroundColor = "white";
-        });
-        // color while dragging across
-        col.addEventListener("mouseover", () => {
-            if (isDrawing) {
-                col.style.backgroundColor = "white";
-            }
-        });
+        for (let j = 1; j <= n; j++) {
+            const col = document.createElement("div");
+            col.classList.add("columnDisplay");
 
-        row.append(col);
+            let opac = 1;
+            //color first square
+            col.addEventListener("mousedown", () => {
+                col.style.backgroundColor = pickColor(opac);
+                if (color === "Dark" && opac > 0) {
+                        opac = opac - 0.1;
+                    }
+            });
+            // color while dragging across
+            col.addEventListener("mouseover", () => {
+                if (isDrawing) {
+                    col.style.backgroundColor = pickColor(opac);
+                    if (color === "Dark" && opac > 0) {
+                        opac = opac - 0.1;
+                    }
+                }
+            });
+
+            row.append(col);
+        }
     }
+}
+
+grid.addEventListener("click", () => {
+    let m = prompt("Enter number of squares per side(1 to 100)");
+    if (m < 1 || m > 100) {
+        alert("Please enter a between [1 to 100]");
+        return;
+    }
+    n = m;
+    deleteGrid(container);
+    gridMaker(n);
+});
+
+reset.addEventListener("click", () => {
+    deleteGrid(container);
+    gridMaker(n);
+});
+
+function deleteGrid(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+penColor.addEventListener("click", () => {
+    // if(color == "Dark") {
+    //     deleteGrid(container);
+
+    // }
+    if (penColor.textContent === "Rainbow") {
+        color = "Rainbow";
+        penColor.textContent = "White";
+    } else {
+        color = "White";
+        penColor.textContent = "Rainbow";
+    }
+});
+
+function pickColor(opac) {
+    let red = 255, green = 255, blue = 255;
+    if (color === "Rainbow") {
+        red = Math.floor(Math.random() * 256);
+        green = Math.floor(Math.random() * 256);
+        blue = Math.floor(Math.random() * 256);
+        opac = 1;
+    } else if(color === "White") opac = 1;
+
+    return `rgba(${red}, ${green}, ${blue}, ${opac})`;
 }
